ded614954de3c2df8a5c7fb5d9a52f0fc0cd0b77
lshpaner <lshpaner@ucla.edu>
2024-12-29 13:21:07 -0800

    added train, model_params, rand patient_ids

--- python_scripts/constants.py
+++ python_scripts/constants.py
@@ -3,4 +3,10 @@
 ################################################################################
 
 dependent_var = "Functional_Outcomes_Bleeding"
-r_state = 222
+rstate = 222
+
+age = ["Age_years"]
+
+mlflow_data = "models/mlflow_exp"
+var_index = "patient_id"
+# , age_bin,
