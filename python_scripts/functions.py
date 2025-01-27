33bbf909882c15ec57f81f744d4eafb2c8228e06
lshpaner <lshpaner@ucla.edu>
2025-01-27 09:52:07 -0800

    Updated weight nomencalture in calculate_bmi_category

--- python_scripts/functions.py
+++ python_scripts/functions.py
@@ -94,7 +94,7 @@ class HealthMetrics:
             if bmi < 18.5:
                 return "Underweight"
             elif 18.5 <= bmi < 24.9:
-                return "Normal weight"
+                return "Normal_Weight"
             elif 25 <= bmi < 29.9:
                 return "Overweight"
             else:
